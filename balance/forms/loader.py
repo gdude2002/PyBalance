a135eb414c7fe77a38a6b398f51d2e101698748d
Gareth Coles <gdude2002@gmail.com>
2019-01-15 17:31:51 +0000

    Document loader

--- balance/forms/loader.py
+++ balance/forms/loader.py
@@ -1,19 +1,30 @@
 import os.path
 import pathlib
-from PySide2.QtCore import QFile
-from PySide2.QtGui import QWindow
 
+from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QWidget
 
 
 class UILoader:
-    def __init__(self):
-        self.path = pathlib.Path(os.path.dirname(__file__))
-        self.loader = QUiLoader()
+    """
+    Provides a simple wrapper around Qt's .ui file loader, which takes the
+    name of a ui file and loads it.
+    """
+    path = pathlib.Path(os.path.dirname(__file__))
+    loader = QUiLoader()
 
-    def load(self, form: str) -> QWindow:
-        ui_path = str(self.path / f"{form}.ui")
+    @classmethod
+    def load(cls, form: str) -> QWidget:
+        """
+        Load up a .ui file and return it.
+
+        :param form: The name of the .ui file to load, without the .ui extension
+        :return: The QWidget created from the UI file
+        """
+
+        ui_path = str(cls.path / f"{form}.ui")
         ui_file = QFile(ui_path)
         ui_file.open(QFile.ReadOnly)
 
-        return self.loader.load(ui_file)
+        return cls.loader.load(ui_file)
